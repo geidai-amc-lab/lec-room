84e6c9ef8c964b866fc8b61bcaede6b6f81c3080
Yutaro <62991215+ytr0@users.noreply.github.com>
2023-04-24 17:08:53 +0900

    feat: Add remote-run.sh

---
+++ 01-init/remote-run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+afplay /System/Library/Sounds/Basso.aiff
+SSID=$( /System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I  | awk -F' SSID: '  '/ SSID: / {print $2}' )
+computer_name=$(scutil --get ComputerName)
+
+show_dialog() {
+osascript << EOF
+on run
+tell app "System Events" to display dialog "[$computer_name] $SSIDに接続しました" buttons {"OK"} default button "OK" giving up after 2
+end run
+EOF
+}
+
+# mount shared drive
+run_osascript() {
+osascript << EOF
+on run
+tell application "Finder"
+    activate
+    tell application "System Events"
+        keystroke "k" using {command down}
+        delay 0.5
+        keystroke return
+        delay 0.5
+        keystroke return
+        delay 2
+    end tell
+end tell
+end run
+EOF
+}
+
+show_dialog
+run_osascript
