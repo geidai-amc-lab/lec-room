41ea232b9143719b26ed85c531355e51d9dbd107
Yutaro <62991215+ytr0@users.noreply.github.com>
2023-04-25 09:56:49 +0900

    fix: refactored

--- 01-init/amcmac-remote-init.sh
+++ 01-init/amcmac-remote-init.sh
@@ -1,39 +1,45 @@
 #!/bin/bash
-afplay /System/Library/Sounds/Basso.aiff
+
+
 SSID=$( /System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I  | awk -F' SSID: '  '/ SSID: / {print $2}' )
 computer_name=$(scutil --get ComputerName)
 
+play_sound() {
+    afplay /System/Library/Sounds/Blow.aiff
+}
+
 show_dialog() {
-osascript << EOF
-on run
-set computer_name to do shell script "scutil --get ComputerName" as text
-set dialog_text to "[$computer_name] " & "$SSID" & "に接続しました" as text
-tell app "System Events" to display dialog dialog_text buttons {"OK"} default button "OK" giving up after 2
-end run
-EOF
+    osascript << EOF
+        on run
+            set computer_name to do shell script "scutil --get ComputerName" as text
+            set dialog_text to "[$computer_name] " & "$SSID" & "に接続しました" as text
+            tell app "System Events" to display dialog dialog_text buttons {"OK"} default button "OK" giving up after 2
+        end run
+    EOF
 }
 
 
 # mount shared drive
 mount_shared() {
-osascript << EOF
-on run
-tell application "Finder"
-    activate
-    tell application "System Events"
-        keystroke "k" using {command down}
-        delay 0.5
-        keystroke return
-        delay 0.5
-        keystroke return
-        delay 2
-    end tell
-end tell
-end run
-EOF
+    osascript << EOF
+        on run
+            tell application "Finder"
+                activate
+                tell application "System Events"
+                    keystroke "k" using {command down}
+                    delay 0.5
+                    keystroke return
+                    delay 0.5
+                    keystroke return
+                    delay 2
+                end tell
+            end tell
+        end run
+    EOF
 }
 
 main() {
+    play_sound
     show_dialog
     mount_shared
 }
