fd1aa00f16d6a180a4809dde8d204cda4ce02625
Yutaro <62991215+ytr0@users.noreply.github.com>
2025-08-29 15:13:02 +0900

    Update setComputerName.sh

--- 01-init/setComputerName.sh
+++ 01-init/setComputerName.sh
@@ -1,8 +1,4 @@
-# 変更したい新しい名前
 NEW_NAME="AMCMAC-001"
-# ComputerName を変更
 sudo scutil --set ComputerName "$NEW_NAME"
-# LocalHostName を変更 (Bonjour 名, xxx.local)
 sudo scutil --set LocalHostName "$NEW_NAME"
-# HostName を変更 (UNIX系コマンドが使う名前)
 sudo scutil --set HostName "$NEW_NAME"
